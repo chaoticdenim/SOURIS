d20958fc5d72205700b8565ed0c958e71f5f9e7a
Guillaume Brizolier <33810550+chaoticdenim@users.noreply.github.com>
2020-02-24 14:16:57 +0100

    update variable names

--- Plot_data.m
+++ Plot_data.m
@@ -1,17 +1,16 @@
 [v,T,vT]=xlsread('Data_for_test.xlsx');
-% 'xlsx' for excel 2007
 %v: Double
 %T and vT : cell
 %use v containing numbers
 t=v(:,1);
-y=v(:,6);
+y=v(:,2);
 %find index of 3 seconds
 lowerBound=3000;
 higherBound=4500;
-[ d, ilx ] = min( abs( t-lowerBound ) );
-[ d, ihx ] = min( abs( t-higherBound ) );
-%get values to average
-vals=y(ilx:ihx);
-F0=mean(vals);
-%if u have to plot second colon depending on first:
+[ startBaseline, ilx ] = min(abs(t-lowerBound));
+[ endBaseline, ihx ] = min(abs(t-higherBound));
+%get baseline
+baseline=y(ilx:ihx);
+F0=mean(baseline);
+%plot F as a function of time
 plot(t/1000,(y-F0)/F0)
