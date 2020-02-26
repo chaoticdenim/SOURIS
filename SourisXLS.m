672f23eb9708297dff4af493dffdcb214d3de393
ArturoM <arturo_mollinedo@hotmail.com>
2020-02-26 11:55:19 +0100

    Update SourisXLS.m

--- SourisXLS.m
+++ SourisXLS.m
@@ -13,22 +13,21 @@ for i = 2:NoOfColumn
         end
     end
 end
-LABEL=cell2mat(TXT(:,2:end));
-LABELF=zeros(1,NoOfColumn-1);
-DATA=NUM(:,2:end);
-DATAF=zeros(NoOfRows,NoOfColumn-1);
-if LABEL(1,1)>LABEL(1,uint8(NoOfColumn/2))
-        k=1;
-        for i = 1:NoOfColumn-1
+LABEL=[NaN,zeros(1,NoOfColumn-1)];
+LABEL(:,2:end)=cell2mat(TXT(:,2:end));
+
+if LABEL(1,2)>LABEL(1,uint8(NoOfColumn/2))
+    DATAF=[NUM(:,1),zeros(NoOfRows,NoOfColumn-1)];    
+    LABELF=[NaN,zeros(1,NoOfColumn-1)];
+        k=0;
+        for i = 2:NoOfColumn
             LABELF(:,i)=LABEL(:,NoOfColumn-k);
-            DATAF(:,i)=DATA(:,NoOfColumn-k);
+            DATAF(:,i)=NUM(:,NoOfColumn-k);
             k=k+1;
         end
+    LABEL=LABELF;
+    NUM=DATAF;
 end
-LABELT=zeros(1,NoOfColumn);
-LABELT(:,:)=[NaN, LABELF];
-DATAT=zeros(NoOfRows,NoOfColumn);
-DATAT(:,:)=[NUM(:,1),DATAF];
-C=[LABELT;DATAT];
 
+C=[double(LABEL);NUM];
 end
