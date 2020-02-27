029faad8276b010d5fb222d8d69716956f7edadc
Guillaume Brizolier <gbrizolier@gmail.com>
2020-02-27 14:00:58 +0100

    histogramme working

--- histogramme.m
+++ histogramme.m
@@ -1,15 +1,15 @@
 function histogramme
-    voltages = {300, 400, 500,600};
-    NoActiveCell = {0, 0, 0, 0};
+    voltages = [300, 400, 500,600];
+    NoActiveCell = zeros(1,4);
     for i = 1:length(voltages)
-        voltage = voltages{i};
+        voltage = voltages(i);
         [time,~, DATAF] = Average5(voltage);
         for roi = 1:229
             res = check_activ(DATAF, time, roi);
             if res == 1
-                NoActiveCell{i} = NoActiveCell{i} + 1;
+                NoActiveCell(i) = NoActiveCell(i) + 1;
             end
         end
     end
-    histogram(NoActiveCell)
+    bar(300:100:600, NoActiveCell)
 end
